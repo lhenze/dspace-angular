cb336196770a3db87f911bad395971dc16775cf5
lhenze <lh435@nyu.edu>
2019-08-01 18:03:08 -0400

    Community page, Another version

---
+++ design/community_page_rethinking_03/includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="navbar navbar-inverse">
+  <div class="footer-holder container">
+    <div class="f-1"><a href="#">About the Archive</a> | <a target="_blank" href="#">Help</a> | <a href="#">Contact</a> | <a href="#">Rights</a> | <a href="#" class="ext" target="_blank">Accessibility <span class="glyphicon glyphicon-new-window"></span></a></div>
+    <div class="f-2">
+      <p _ngcontent-c5="">
+        <a _ngcontent-c5="" href="http://www.dspace.org/">DSpace software</a> copyright © 2002-2019 <a _ngcontent-c5="" href="http://www.duraspace.org/">DuraSpace</a>
+      </p>
+    </div>
+  </div>
+</footer>
