1af357ebf36458c4baaa1c6d6bfed4c384d893a3
lhenze <lh435@nyu.edu>
2019-08-12 11:50:23 -0400

    Design files moved to a dedicated repo

--- design/community_page_rethinking_03/includes/sidebar_browse.php
+++
@@ -1,9 +0,0 @@
-<div class="sidebar-item">
-  <h3 class="item-title">Browse all items</h3>
-  <div class="list-group">
-    <a class="list-group-item" href="/browse/title?scope=fa49e785-f9af-4a88-8f55-6345aa42c4d1">By Title</a>
-    <a class="list-group-item" href="/browse/dateissued?scope=fa49e785-f9af-4a88-8f55-6345aa42c4d1">By Issue Date</a>
-    <a class="list-group-item" href="/browse/author?scope=fa49e785-f9af-4a88-8f55-6345aa42c4d1">By Author</a>
-    <a class="list-group-item" href="/browse/subject?scope=fa49e785-f9af-4a88-8f55-6345aa42c4d1">By Subject</a>
-  </div>
-</div>
--- design/community_page_rethinking_03/includes/sidebar_news.php
+++
@@ -1,6 +0,0 @@
-<div class="sidebar-item">
-  <div class="sidebar-header">
-    <h3 class="item-title">Downloadable EBooks</h3>
-  </div>
-  <p>This is the optional "News" section. It provides a space for arbitrary HTML for both the title of this section and its content. <a href="#" style="white-space: nowrap;">This&nbsp;is a link.</a></p>
-</div>
